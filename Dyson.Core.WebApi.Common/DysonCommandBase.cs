312e636cdef6f5270807d2d22cb42e26bc6e9f6b
LYMDS <lym_ds@163.com>
2020-08-12 18:01:57 +0800

    测试成功，准备动态依赖注入逻辑类

--- Dyson.Core.WebApi.Common/DysonCommandBase.cs
+++ Dyson.Core.WebApi.Common/DysonCommandBase.cs
@@ -17,7 +17,7 @@ namespace Dyson.Core.WebApi.Common
         /// </summary>
         public DysonCommandBase() 
         {
-            
+            this.LogManager = null;
         }
 
         // 日志操作器
@@ -36,7 +36,7 @@ namespace Dyson.Core.WebApi.Common
             var db = new SqlSugarClient(
                 new ConnectionConfig()
                 {
-                    ConnectionString = "server=.;uid=sa;pwd=**********;database=CSRZIC_MSCRM",
+                    ConnectionString = "server=.;uid=sa;pwd=collecting123;database=CSRZIC_MSCRM",
                     DbType = DbType.SqlServer,// 设置数据库类型
                     IsAutoCloseConnection = true,// 自动释放数据务，如果存在事务，在事务结束后释放
                     InitKeyType = InitKeyType.Attribute // 从实体特性中读取主键自增列信息
@@ -45,8 +45,12 @@ namespace Dyson.Core.WebApi.Common
             // 执行SQL时的切面AOP
             db.Aop.OnLogExecuting = (sql, pars) =>
             {
-                string debugSQL = sql + "\r\n" + db.Utilities.SerializeObject(pars.ToDictionary(it => it.ParameterName, it => it.Value));
-                LogManager.LogDebug(debugSQL);
+                // 如果外部Command不使用日志
+                if (this.LogManager != null) 
+                {
+                    string debugSQL = sql + "\r\n" + db.Utilities.SerializeObject(pars.ToDictionary(it => it.ParameterName, it => it.Value));
+                    this.LogManager.LogDebug(debugSQL); 
+                }
             };
             return db;
         }
